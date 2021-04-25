be205686cbc8759e6f047c0c7a7b5b720663a2b6
fangchenjiang <fangchenjiang@37.com>
2021-04-25 17:46:32 +0800

    网关负载均衡带参数访问

--- xf-order-8001/src/main/java/com/xf/gz/order/controllrt/OrderController.java
+++ xf-order-8001/src/main/java/com/xf/gz/order/controllrt/OrderController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -67,5 +68,10 @@ public class OrderController {
             e.printStackTrace();
         }
         return "请求到了订单服务,端口号:"+port;
+    }
+
+    @GetMapping("/greeting")
+    public String sayHello(@RequestParam("name")String name){
+        return port+": hello,"+name;
     }
 }
--- xf-order-8002/src/main/java/com/gz/xf/controller/OrderController.java
+++ xf-order-8002/src/main/java/com/gz/xf/controller/OrderController.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -50,5 +51,10 @@ public class OrderController {
     @GetMapping("/getPort")
     public String getPort(){
         return "请求到了订单服务,端口号:"+port;
+    }
+
+    @GetMapping("/greeting")
+    public String sayHello(@RequestParam("name")String name){
+        return port+": hello,"+name;
     }
 }
